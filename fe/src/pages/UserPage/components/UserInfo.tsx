f6ddf4822341f89cf3aa4b7391ced0f748c333a6
quynhuw <qinhuuuuu@gmail.com>
2024-04-30 18:55:49 +0700

    add func change user info

--- fe/src/pages/UserPage/components/UserInfo.tsx
+++ fe/src/pages/UserPage/components/UserInfo.tsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import clsx from "clsx";
+import { UserType } from "../../../type/type";
+import { save_change, initUser, userReducer } from "../store";
 
 const UserInfo = () => {
+  const [user, dispatch] = useReducer(userReducer, initUser);
+  const [tempUser, setTempUser] = useState<UserType>(user);
   const [visibleForm, setVisibleForm] = useState(false);
-  const handleClickUpdate = () => {
+  const handleClickCancel = () => {
+    setTempUser(user);
+    setVisibleForm(!visibleForm);
+  };
+  const handleClickSave = () => {
+    dispatch(save_change(tempUser));
     setVisibleForm(!visibleForm);
   };
 
   const modalClass = clsx(
-    "bg-gray-900 w-[550px] h-fit p-5 rounded flex flex-col gap-3 ",
+    "bg-quaternary w-[550px] h-fit p-5 rounded flex flex-col gap-3 text-primary",
     visibleForm
       ? "animate-update-form translate-y-1/2"
       : "animate-update-form-closed "
@@ -16,74 +25,87 @@ const UserInfo = () => {
   return (
     <div className="flex flex-col w-3/4 gap-5 p-5">
       <div className="flex flex-col gap-3">
-        <div className="text-[22px] font-light uppercase">
-          Thông tin cá nhân
-        </div>
-        <div className=" bg-gray-900 h-[2px] rounded-md"></div>
+        <div className="text-[22px]  uppercase">Thông tin cá nhân</div>
+        <div className=" bg-primary h-[2px] rounded-md"></div>
       </div>
-      <div className="flex flex-col gap-1 font-light">
-        <div>Phan Thi Quynh Nhu</div>
-        <div>31/12/2002</div>
-        <div>hallo@gmail.com</div>
-        <div>0819555888</div>
+      <div className="flex flex-col gap-1 ">
+        <div>{user.name}</div>
+        <div>{user.birth}</div>
+        <div>{user.email}</div>
+        <div>{user.phone}</div>
       </div>
       <div>
         <label
-          onClick={handleClickUpdate}
+          onClick={() => setVisibleForm(!visibleForm)}
           htmlFor="update-form"
-          className="px-8 py-3 text-white bg-gray-900 rounded cursor-pointer hover:bg-gray-800 w-fit h-fit"
+          className="px-8 py-3 text-white rounded cursor-pointer bg-primary hover:bg-secondary w-fit h-fit"
         >
           Cập nhật
         </label>
         {visibleForm && (
           <div
             id="update-form"
-            className="fixed top-0 left-0 z-10 flex content-center justify-center w-full h-screen overflow-hidden bg-opacity-50 bg-slate-600"
+            className="fixed top-0 left-0 z-10 flex content-center justify-center w-full h-screen overflow-hidden bg-opacity-50 bg-primary"
           >
             <div className={modalClass}>
-              <div className="mb-2 text-2xl font-light text-center uppercase">
+              <div className="mb-2 text-2xl text-center uppercase">
                 Thông tin cá nhân
               </div>
               <div className="flex items-center gap-6">
-                <div className="w-1/4 font-light">Họ tên</div>
+                <div className="w-1/4 ">Họ tên</div>
                 <input
                   type="text"
-                  value={"Phan Thi Quynh Nhu"}
-                  className="w-2/3 p-2 text-gray-900 rounded outline-none"
+                  value={tempUser.name}
+                  onChange={(e) =>
+                    setTempUser((prev) => ({ ...prev, name: e.target.value }))
+                  }
+                  className="w-2/3 p-2 bg-white border rounded outline-none border-primary"
                 />
               </div>
               <div className="flex items-center gap-6">
-                <div className="w-1/4 font-light">Ngày sinh</div>
+                <div className="w-1/4 ">Ngày sinh</div>
                 <input
                   type="date"
-                  value="2002-12-31"
-                  className="w-2/3 p-2 text-gray-900 rounded outline-none"
+                  value={tempUser.birth}
+                  onChange={(e) =>
+                    setTempUser((prev) => ({ ...prev, birth: e.target.value }))
+                  }
+                  className="w-2/3 p-2 pr-4 bg-white border rounded outline-none border-primary"
                 />
               </div>
               <div className="flex items-center gap-6">
-                <div className="w-1/4 font-light">Email</div>
+                <div className="w-1/4 ">Email</div>
                 <input
                   type="text"
-                  value={"qinhuuuuu@gmail.com"}
-                  className="w-2/3 p-2 text-gray-900 rounded outline-none"
+                  value={tempUser.email}
+                  onChange={(e) =>
+                    setTempUser((prev) => ({ ...prev, email: e.target.value }))
+                  }
+                  className="w-2/3 p-2 bg-white border rounded outline-none border-primary"
                 />
               </div>
               <div className="flex items-center gap-6">
-                <div className="w-1/4 font-light">Số điện thoại</div>
+                <div className="w-1/4 ">Số điện thoại</div>
                 <input
                   type="text"
-                  value={"0939666444"}
-                  className="w-2/3 p-2 text-gray-900 rounded outline-none"
+                  value={tempUser.phone}
+                  onChange={(e) =>
+                    setTempUser((prev) => ({ ...prev, phone: e.target.value }))
+                  }
+                  className="w-2/3 p-2 bg-white border rounded outline-none border-primary"
                 />
               </div>
               <div className="flex justify-end gap-5 mt-3 mr-5">
                 <div
-                  onClick={handleClickUpdate}
-                  className="px-5 py-2 text-gray-900 bg-white rounded cursor-pointer hover:bg-gray-500"
+                  onClick={() => handleClickCancel()}
+                  className="px-5 py-2 text-white rounded cursor-pointer bg-primary hover:bg-secondary"
                 >
                   Huỷ
                 </div>
-                <div className="px-5 py-2 text-gray-900 bg-white rounded cursor-pointer hover:bg-gray-500">
+                <div
+                  onClick={() => handleClickSave()}
+                  className="px-5 py-2 text-white rounded cursor-pointer bg-primary hover:bg-secondary"
+                >
                   Lưu thay đổi
                 </div>
               </div>
